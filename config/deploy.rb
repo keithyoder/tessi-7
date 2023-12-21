3e6b6684ce90afa13c796d70bf0a040a82c02b80
keithyoder <yoder@tessi.com.br>
2023-12-21 15:56:15 -0300

    fix dependencies

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.17.0'
+lock '~> 3.18.0'
 
 # set :application, "my_app_name"
 # set :repo_url, "git@example.com:me/my_repo.git"

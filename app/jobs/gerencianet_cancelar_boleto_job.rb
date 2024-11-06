78992a58f73f14023cff40146fb9026e2c6b0b42
keithyoder <yoder@tessi.com.br>
2024-11-06 07:28:28 -0300

    fix icons

--- app/jobs/gerencianet_cancelar_boleto_job.rb
+++ app/jobs/gerencianet_cancelar_boleto_job.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-class GerencianetCancelarBoletoJob < ApplicationJob
-  
+class GerencianetCancelarBoletoJob < ApplicationJob # rubocop:disable Style/Documentation
+  queue_as :default
+
   def perform(id_externo)
     params = {
       id: id_externo
     }
-      
-    GerencianetClient.cliente.cancel_charge(params: params)
+
+    GerencianetClient.cliente.cancelCharge(params: params)
   end
 end
-  

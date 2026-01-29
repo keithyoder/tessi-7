5b263a10f16fe3ff5b34416f02c199eb4aefa296
keithyoder <yoder@tessi.com.br>
2026-01-28 23:20:15 -0300

    more rubocop refactoring

--- app/controllers/ip_redes_controller.rb
+++ app/controllers/ip_redes_controller.rb
@@ -1,77 +1,53 @@
 # frozen_string_literal: true
 
 class IpRedesController < ApplicationController
-  before_action :set_ip_rede, only: %i[show edit update destroy]
   load_and_authorize_resource
 
   # GET /ip_redes
-  # GET /ip_redes.json
   def index
-    @q = IpRede.ransack(params[:q])
-    @q.sorts = ['rede']
-    @ip_redes = @q.result.page params[:page]
+    @q = IpRede
+      .accessible_by(current_ability)
+      .ransack(params[:q])
+
+    @q.sorts = ['rede'] if @q.sorts.empty?
+    @ip_redes = @q.result.page(params[:page])
   end
 
   # GET /ip_redes/1
-  # GET /ip_redes/1.json
   def show; end
 
   # GET /ip_redes/new
-  def new
-    @ip_rede = IpRede.new
-  end
+  def new; end
 
   # GET /ip_redes/1/edit
   def edit; end
 
   # POST /ip_redes
-  # POST /ip_redes.json
   def create
-    @ip_rede = IpRede.new(ip_rede_params)
-
-    respond_to do |format|
-      if @ip_rede.save
-        format.html { redirect_to @ip_rede, notice: 'Rede IP criada com sucesso.' }
-        format.json { render :show, status: :created, location: @ip_rede }
-      else
-        format.html { render :new }
-        format.json { render json: @ip_rede.errors, status: :unprocessable_entity }
-      end
+    if @ip_rede.save
+      redirect_to @ip_rede, notice: t('.notice')
+    else
+      render :new, status: :unprocessable_content
     end
   end
 
   # PATCH/PUT /ip_redes/1
-  # PATCH/PUT /ip_redes/1.json
   def update
-    respond_to do |format|
-      if @ip_rede.update(ip_rede_params)
-        format.html { redirect_to @ip_rede, notice: 'Rede IP atualizada com sucesso.' }
-        format.json { render :show, status: :ok, location: @ip_rede }
-      else
-        format.html { render :edit }
-        format.json { render json: @ip_rede.errors, status: :unprocessable_entity }
-      end
+    if @ip_rede.update(ip_rede_params)
+      redirect_to @ip_rede, notice: t('.notice')
+    else
+      render :edit, status: :unprocessable_content
     end
   end
 
   # DELETE /ip_redes/1
-  # DELETE /ip_redes/1.json
   def destroy
     @ip_rede.destroy
-    respond_to do |format|
-      format.html { redirect_to ip_redes_url, notice: 'Ip rede was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to ip_redes_url, notice: t('.notice')
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_ip_rede
-    @ip_rede = IpRede.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
   def ip_rede_params
     params.require(:ip_rede).permit(:rede, :ponto_id, :subnet)
   end
--- app/controllers/pagamento_perfis_controller.rb
+++ app/controllers/pagamento_perfis_controller.rb
@@ -2,35 +2,37 @@
 
 class PagamentoPerfisController < ApplicationController
   load_and_authorize_resource
-  before_action :set_pagamento_perfil, only: %i[show edit update destroy remessa]
+  before_action :set_pagamento_perfil, only: %i[remessa]
 
   # GET /pagamento_perfis
-  # GET /pagamento_perfis.json
   def index
-    @pagamento_perfis = PagamentoPerfil.order('nome').all
+    @pagamento_perfis = PagamentoPerfil.accessible_by(current_ability).order(:nome)
   end
 
   # GET /pagamento_perfis/1
-  # GET /pagamento_perfis/1.json
   def show
-    @retornos = @pagamento_perfil.retornos.order(data: :desc).page params[:page]
+    @retornos = @pagamento_perfil.retornos.order(data: :desc).page(params[:page])
+
     @faturas = @pagamento_perfil.faturas
-                                .eager_load(:contrato, :pessoa)
-                                .em_aberto.where('vencimento < ?', 1.week.from_now)
-                                .order(:vencimento)
-                                .ransack.result.page(params[:page])
+      .accessible_by(current_ability)
+      .eager_load(:contrato, :pessoa)
+      .em_aberto
+      .where(vencimento: ...1.week.from_now)
+      .order(:vencimento)
+      .ransack.result
+      .page(params[:page])
   end
 
   # GET /pagamento_perfis/new
-  def new
-    @pagamento_perfil = PagamentoPerfil.new
-  end
+  def new; end
 
   # GET /pagamento_perfis/1/edit
   def edit; end
 
+  # GET /pagamento_perfis/1/remessa
   def remessa
     @pagamento_perfil.update!(sequencia: params[:sequencia])
+
     send_data(
       @pagamento_perfil.remessa(params[:sequencia]).gera_arquivo,
       content_type: 'text/plain',
@@ -40,57 +42,35 @@ class PagamentoPerfisController < ApplicationController
   end
 
   # POST /pagamento_perfis
-  # POST /pagamento_perfis.json
   def create
-    @pagamento_perfil = PagamentoPerfil.new(pagamento_perfil_params)
-
-    respond_to do |format|
-      if @pagamento_perfil.save
-        format.html { redirect_to @pagamento_perfil, notice: 'Pagamento perfil was successfully created.' }
-        format.json { render :show, status: :created, location: @pagamento_perfil }
-      else
-        format.html { render :new }
-        format.json { render json: @pagamento_perfil.errors, status: :unprocessable_entity }
-      end
+    if @pagamento_perfil.save
+      redirect_to @pagamento_perfil, notice: t('.notice')
+    else
+      render :new, status: :unprocessable_content
     end
   end
 
   # PATCH/PUT /pagamento_perfis/1
-  # PATCH/PUT /pagamento_perfis/1.json
   def update
-    respond_to do |format|
-      if @pagamento_perfil.update(pagamento_perfil_params)
-        format.html { redirect_to @pagamento_perfil, notice: 'Pagamento perfil was successfully updated.' }
-        format.json { render :show, status: :ok, location: @pagamento_perfil }
-      else
-        format.html { render :edit }
-        format.json { render json: @pagamento_perfil.errors, status: :unprocessable_entity }
-      end
+    if @pagamento_perfil.update(pagamento_perfil_params)
+      redirect_to @pagamento_perfil, notice: t('.notice')
+    else
+      render :edit, status: :unprocessable_content
     end
   end
 
   # DELETE /pagamento_perfis/1
-  # DELETE /pagamento_perfis/1.json
   def destroy
     @pagamento_perfil.destroy
-    respond_to do |format|
-      format.html { redirect_to pagamento_perfis_url, notice: 'Pagamento perfil was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to pagamento_perfis_url, notice: t('.notice')
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_pagamento_perfil
-    @pagamento_perfil = PagamentoPerfil.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
   def pagamento_perfil_params
     params.require(:pagamento_perfil).permit(
-      :nome, :tipo, :cedente, :agencia, :conta, :carteira, :banco, :conta,
-      :variacao, :sequencia, :ativo
+      :nome, :tipo, :cedente, :agencia, :conta, :carteira,
+      :banco, :variacao, :sequencia, :ativo
     )
   end
 end

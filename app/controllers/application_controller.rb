e098784f08e8cbefaceebb1e3f5499c135c80439
keithyoder <yoder@tessi.com.br>
2026-01-31 09:13:46 -0300

    remove debugging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  before_action :debug_csrf
   check_authorization unless: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
@@ -12,21 +11,5 @@ class ApplicationController < ActionController::Base
       format.html { redirect_to main_app.root_url, notice: exception.message }
       format.js   { head :forbidden, content_type: 'text/html' }
     end
-  end
-
-  private
-
-  def debug_csrf
-    return unless devise_controller?
-
-    Rails.logger.info '=' * 80
-    Rails.logger.info "CSRF DEBUG - Controller: #{controller_name}##{action_name}"
-    Rails.logger.info "Secret key (first 20): #{Rails.application.secret_key_base&.[](0..19) || 'MISSING!'}"
-    Rails.logger.info "Session ID: #{session.id}"
-    Rails.logger.info "Session CSRF Token: #{session[:_csrf_token]}"
-    Rails.logger.info "Request.ssl?: #{request.ssl?}"
-    Rails.logger.info "Cookie value: #{cookies.encrypted[Rails.application.config.session_options[:key]]&.[](0..50)}"
-    Rails.logger.info "Session enabled?: #{request.session_options}"
-    Rails.logger.info '=' * 80
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,22 +2,5 @@
 
 module Users
   class SessionsController < Devise::SessionsController
-    def new
-      # Force write something to session
-      session[:test] = 'hello'
-      Rails.logger.info "Session test value: #{session[:test]}"
-      Rails.logger.info "Session ID after write: #{session.id}"
-      super
-    end
-
-    # DELETE /users/sign_out
-    def destroy
-      signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-      set_flash_message! :notice, :signed_out if signed_out
-
-      respond_to do |format|
-        format.html { redirect_to after_sign_out_path_for(resource_name), status: :see_other }
-      end
-    end
   end
 end

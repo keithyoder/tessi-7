1eba96d99a02ead2772d451d6818ee1b3270f495
keithyoder <yoder@tessi.com.br>
2026-01-31 08:05:05 -0300

    debug app controller'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :debug_csrf
   check_authorization unless: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
@@ -11,5 +12,20 @@ class ApplicationController < ActionController::Base
       format.html { redirect_to main_app.root_url, notice: exception.message }
       format.js   { head :forbidden, content_type: 'text/html' }
     end
+  end
+
+  private
+
+  def debug_csrf
+    return unless devise_controller?
+
+    Rails.logger.info '=' * 80
+    Rails.logger.info "CSRF DEBUG - Controller: #{controller_name}##{action_name}"
+    Rails.logger.info "Session ID: #{session.id}"
+    Rails.logger.info "Session CSRF Token: #{session[:_csrf_token]}"
+    Rails.logger.info "Form Authenticity Token: #{form_authenticity_token}"
+    Rails.logger.info "Params Token: #{params[:authenticity_token]}"
+    Rails.logger.info "Request Format: #{request.format}"
+    Rails.logger.info '=' * 80
   end
 end

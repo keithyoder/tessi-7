8cdc07fed60fcb184124f23ce42163ec3e0d284e
keithyoder <yoder@tessi.com.br>
2026-02-12 12:16:28 -0300

    remove unused module

--- app/models/banco_do_brasil/cobranca.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module BancoDoBrasil
-  class Cobranca < BancoDoBrasil
-    base_uri 'https://api.hm.bb.com.br/cobrancas/v2'
-  end
-end
--- app/models/banco_do_brasil/pix.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module BancoDoBrasil
-  class Pix < BancoDoBrasil
-    base_uri 'https://api.sandbox.bb.com.br/pix/v1/'
-  end
-end

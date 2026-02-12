e9f3eb786cdadeba082e88f8e602ce32b946a201
keithyoder <yoder@tessi.com.br>
2026-02-12 17:58:33 -0300

    update snmp reader

--- app/services/ubiquiti/snmp_reader.rb
+++ app/services/ubiquiti/snmp_reader.rb
@@ -15,23 +15,25 @@ module Ubiquiti
       modelo: '1.2.840.10036.3.1.2.1.3.5',
       firmware: '1.2.840.10036.3.1.2.1.4.5',
       sys_descr: '1.3.6.1.2.1.1.1.0',
-      mac: '1.3.6.1.2.1.2.2.1.6.2'
+      mac: '1.3.6.1.2.1.2.2.1.6.2',
+      mac_ubnt: '1.3.6.1.4.1.41112.1.4.5.1.4.1'
     }.freeze
 
     COMMUNITY = Provisioner::SNMP_COMMUNITY
 
-    attr_reader :ponto
+    attr_reader :device
 
-    def initialize(ponto)
-      @ponto = ponto
+    def initialize(device)
+      @device = device
     end
 
     def coletar_informacoes
       with_snmp_manager do |manager|
         response = manager.get(OIDS.values)
-        parse_response(response)
         result = parse_response(response)
         result[:modelo] = resolve_modelo(result)
+        result[:mac] = resolve_mac(result)
+        result[:firmware] = resolve_firmware(result)
         result
       end
     end
@@ -64,7 +66,7 @@ module Ubiquiti
 
     def snmp_config
       {
-        host: ponto.ip.to_s,
+        host: device.ip.to_s,
         community: COMMUNITY,
         port: 161,
         version: :SNMPv1,
@@ -77,12 +79,39 @@ module Ubiquiti
       modelo = result[:modelo]
       return modelo if valid_snmp_value?(modelo)
 
-      # Walk the parent OID to find the model regardless of index
       with_snmp_manager do |manager|
         manager.walk('1.2.840.10036.3.1.2.1.3') do |vb|
           return vb.value.to_s if valid_snmp_value?(vb.value.to_s)
         end
       end
+
+      nil
+    end
+
+    def resolve_firmware(result)
+      fw = result[:firmware]
+      return fw if valid_snmp_value?(fw)
+
+      with_snmp_manager do |manager|
+        manager.walk('1.2.840.10036.3.1.2.1.4') do |vb|
+          return vb.value.to_s if valid_snmp_value?(vb.value.to_s)
+        end
+      end
+
+      nil
+    end
+
+    def resolve_mac(result)
+      mac = format_mac_value(result[:mac])
+      return mac if mac.present?
+
+      format_mac_value(result[:mac_ubnt])
+    end
+
+    def format_mac_value(value)
+      return nil if value.blank? || !valid_snmp_value?(value)
+
+      value.to_s.bytes.map { |b| '%02X' % b }.join(':')
     end
 
     def parse_response(response)
